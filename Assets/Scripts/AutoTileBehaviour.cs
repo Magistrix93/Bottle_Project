72351981438505a8b3b9a97b51293d2e07e303ea
Magistrix93 <simonebalbi93@libero.it>
2016-10-25 22:45:13 +0200

    Simone - Scena 1 lightmap

---
+++ Assets/Scripts/AutoTileBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoTileBehaviour : MonoBehaviour
+{
+    private Material[] materials;
+
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        SetTiling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetTiling()
+    {        
+        rend.material.mainTextureScale = (new Vector2(transform.parent.localScale.x, transform.parent.localScale.z));
+    }
+}
+
