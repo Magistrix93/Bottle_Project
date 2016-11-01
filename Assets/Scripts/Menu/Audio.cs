33cac515e70e0a2ddfa701d4b3e94094206f723a
red59 <alerossi89@hotmail.it>
2016-11-01 16:53:35 +0100

    ale update

---
+++ Assets/Scripts/Menu/Audio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Audio : MonoBehaviour {
+
+    private AudioSource audio;
+
+    public Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+
+        audio = GetComponent<AudioSource>();
+	
+	}
+
+    public void changeVolume()
+    {
+        audio.volume = volumeSlider.value;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
