6b29552a72ee5e28e8f126a3dfccb5dccce1e4c8
R1ddik <r1ddik1990@hotmail.it>
2016-11-02 21:14:27 +0100

    alessio

---
+++ Assets/Scripts/CartaCestino.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using PlayMaker;
+
+public class CartaCestino : MonoBehaviour
+{
+
+    private PlayMakerFSM myFsm;
+    private bool cartaOk;
+
+    // Use this for initialization
+    void Start()
+    {
+        myFsm = GetComponent<PlayMakerFSM>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cartaOk = myFsm.FsmVariables.GetFsmBool("StartOpen").Value;
+        if(cartaOk)
+        {
+
+        }
+    }
+}
